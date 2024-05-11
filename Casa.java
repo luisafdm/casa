63747efe9f4935f6eedc376657bb38b4282e7f17
luisafdm <125517336+luisafdm@users.noreply.github.com>
2024-05-10 21:21:35 -0500

    si se

--- Casa.java
+++ Casa.java
@@ -1,8 +1,12 @@
 public class Casa {
     public static void main (String[] args){
         sala();
+        habitacion();
     }
     static void sala(){
         System.out.println("Estoy viendo tv");
+    }
+    static void habitacion(){
+        System.out.println("Voy a dormir");
     }
 }
